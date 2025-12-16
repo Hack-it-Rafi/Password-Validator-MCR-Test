68c16a2a074b0722ac854e4e59dbdb8710a00fb6
Hack-it-Rafi <zozorafi2016@gmail.com>
2025-12-17 03:57:10 +0600

    Added uppercase and digit checks based on review

--- validator.py
+++ validator.py
@@ -1,9 +1,11 @@
 def check_password(password):
-    # TODO: Add more checks later
-    if len(password) > 8:
-        return True
-    else:
+    if len(password) <= 8:
         return False
+
+    has_upper = any(char.isupper() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    return has_upper and has_digit
 
 if __name__ == "__main__":
     pwd = input("Enter a password to check: ")
